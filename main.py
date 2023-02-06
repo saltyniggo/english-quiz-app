fae6a3bc214b8fdf8359dfbd87723e53775f4767
nicoschulz <n.suraimu@proton.me>
2023-02-06 09:03:46 +0100

    added a few comments and a ranking system

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# import the modules
 import csv
 import random
 import subprocess
 
+# print a welcome question
 print('Welcome to my quiz')
 print('GOOD LUCK!')
 print('')
 
+#
 def read_questions():
     with open('questions.csv', 'r') as f:
         reader = csv.DictReader(f)
@@ -16,7 +19,6 @@ score = 0
 for question in questions:
     options = [question['Option1'], question['Option2'], question['Option3'], question['Option4']]
     random.shuffle(options)
- #   subprocess.call("clear", shell=True)
     print(question['Question'])
     for i, option in enumerate(options):
         print(f"{i+1}. {option}")
@@ -29,4 +31,16 @@ for question in questions:
         subprocess.call("clear", shell=True)
         print("Incorrect.")
 
+# ranking system for the user
+if score == len(questions):
+    rank = "perfect"
+elif score >= len(questions) * 0.8:
+    rank = "good"
+elif score >= len(questions) * 0.5:
+    rank = "medium"
+else:
+    rank = "bad"
+
+# prints the result and the rank of the user
 print(f"You got {score} out of {len(questions)} questions correct.")
+print(f"Your english skills in this topic are {rank}.")
